98908a779e7296ee3c1b6232430efcebc3994336
zeinaaldarf01-pixel <zeinaaldarf01@gmail.com>
2025-12-09 21:17:36 +0100

    task3

--- #Python 9.py
+++ #Python 9.py
@@ -1,6 +1,17 @@
 #Python 9.12
-
-i = 0
-while i < 5:
-    print("Iteration:", i)
-    i += 1
+i = 1
+while i <= 10:
+ print(i)
+ i += 1
+ #task 2
+ i = 10
+while i >= 0:
+ print(i)
+i -= 1  
+print("Blast off!")
+#task3
+password = input("Enter password: ")
+while password != "python123":
+ print("Wrong password, try again.")
+ password = input("Enter password: ")
+print("Access granted")
